44de35af7acbdbab4e38401ff9a50abc4f5d2db1
ed_he <ed_he@syntrontech.com>
2018-02-22 14:05:10 +0800

    list , enum ,spilt

---
+++ src/sideProjectTest/DoubleMIN.java
@@ -0,0 +1,9 @@
+package sideProjectTest;
+
+public class DoubleMIN {
+
+    public static void main(String[] args) {
+
+        System.out.println(Double.MIN_VALUE > -1);
+    }
+}
---
+++ src/sideProjectTest/ListToArraytoList.java
@@ -0,0 +1,22 @@
+package sideProjectTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class ListToArraytoList {
+
+    public static void main(String[] args) {
+
+        Integer[] a= {1,2,3,4};
+        List<Integer> al = new ArrayList<Integer>(asList(1, 2, 3, 5, 7));
+        ArrayList<Integer> aa = new ArrayList<Integer>(asList(1,2,3));
+        List<Integer> cc = Arrays.asList(a);
+        System.out.println(cc);
+        System.out.println(al);
+        System.out.println(aa);
+
+    }
+}

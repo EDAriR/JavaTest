1fb7538d6e73e1729f2f460828ff0c0bc18ba076
ed_he <ed_he@syntrontech.com>
2018-04-30 15:53:14 +0800

    add local in amount report properties

--- src/sideProjectTest/ResourceBundleTest.java
+++ src/sideProjectTest/ResourceBundleTest.java
@@ -6,7 +6,7 @@ public class ResourceBundleTest {
 
     public static void main(String[] args) {
 
-//        native2ascii -encoding UTF-8 age_report.properties language2_zh_tw.properties
+//      native2ascii -encoding UTF-8 amount_report.properties amount_report_zh_tw.properties
 
         ResourceBundle res = ResourceBundle.getBundle("properties.language2_zh_tw");
         System.out.print(res.getString("under_old_abnormal_bloodpressure_rate") + "!");

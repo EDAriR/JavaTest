cb69a95ba857a0a3fe9c38383e6c16c5791b84b9
ed_he <ed_he@syntrontech.com>
2018-07-26 23:52:17 +0800

    upperCase

---
+++ src/sideProjectTest/StrtoUpperCase.java
@@ -0,0 +1,15 @@
+package sideProjectTest;
+
+public class StrtoUpperCase {
+
+    public static void main(String[] args) {
+
+        String str = "aaa1";
+
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+        System.out.println(str);
+        String ss = str.toUpperCase();
+        System.out.println(ss);
+    }
+}
